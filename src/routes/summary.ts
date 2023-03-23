f133034dea71f6f81c5102899a7b724e792a701d
james5418 <james900425@gmail.com>
2023-03-23 10:52:38 +0800

    [feat] allow users to use their own key

--- src/routes/summary.ts
+++ src/routes/summary.ts
@@ -2,13 +2,16 @@ import { Router, Request, Response } from 'express';
 import { Configuration, OpenAIApi } from 'openai';
 import { config } from '../config';
 
+
 export const summaryRouter = Router();
+
+const MODEL: string = "gpt-3.5-turbo-0301"
 
 const configuration = new Configuration({
     apiKey: config.openaiApiKey,
 });
-
 const openai = new OpenAIApi(configuration);
+
 
 summaryRouter.post('/', async(req: Request, res: Response): Promise<void> => {
     try{
@@ -16,7 +19,37 @@ summaryRouter.post('/', async(req: Request, res: Response): Promise<void> => {
         const content: string = req.body.content;
 
         const completion = await openai.createChatCompletion({
-            model: "gpt-3.5-turbo-0301",
+            model: MODEL,
+            messages: [
+                {"role": "system", "content": prompt},
+                {"role": "user", "content": content},
+            ],
+        });
+        res.status(200).json(completion.data.choices[0].message?.content);
+
+    }catch(error: any){
+        if(error.response){
+            res.status(error.response.status).json(error.response.data);
+        }else{
+            res.status(500).json(error.message);
+        }
+    }
+});
+
+
+summaryRouter.post('/key', async(req: Request, res: Response): Promise<void> => {
+    try{
+        const prompt: string = req.body.prompt;
+        const content: string = req.body.content;
+        const key: string = req.body.key;
+
+        const userConfiguration = new Configuration({
+            apiKey: key,
+        });
+        const userOpenai = new OpenAIApi(userConfiguration);
+
+        const completion = await userOpenai.createChatCompletion({
+            model: MODEL,
             messages: [
                 {"role": "system", "content": prompt},
                 {"role": "user", "content": content},
--- tests/summary.test.ts
+++ tests/summary.test.ts
@@ -1,5 +1,6 @@
 import request from 'supertest';
-import server from "../src/index";
+import server from '../src/index';
+import { config } from '../src/config';
 
 
 afterAll(async () => {
@@ -11,10 +12,51 @@ describe("POST /openai/summary", () => {
     test('return a summary for a given prompt and content', async () => {
         const response = await request(server)
             .post('/openai/summary')
-            .send({ prompt: "You are a helpful assistant.", content: "How are you?"});
+            .send({ prompt: "You are a helpful assistant.", content: "How are you?" });
 
         expect(response.status).toBe(200);
         expect(typeof response.body).toBe('string');
+    });
+
+    test('not provide prompt or content', async () => {
+        const response = await request(server)
+            .post('/openai/summary')
+            .send({});
+
+        expect(response.status).toBe(400);
+        expect(response.body.error.message).toEqual(expect.stringContaining("content' is a required property"));
+    });
+
+});
+
+
+describe("POST /openai/summary/key", () => {
+
+    test('return a summary for a given prompt, content, and key', async () => {
+        const response = await request(server)
+            .post('/openai/summary/key')
+            .send({ prompt: "You are a helpful assistant.", content: "How are you?", key: config.openaiApiKey });
+
+        expect(response.status).toBe(200);
+        expect(typeof response.body).toBe('string');
+    });
+
+    test('not provide prompt or content', async () => {
+        const response = await request(server)
+            .post('/openai/summary/key')
+            .send({ key: config.openaiApiKey });
+
+        expect(response.status).toBe(400);
+        expect(response.body.error.message).toEqual(expect.stringContaining("content' is a required property"));
+    });
+
+    test('invalid api key', async () => {
+        const response = await request(server)
+          .post('/openai/summary/key')
+          .send({ prompt: "You are a helpful assistant.", content: "How are you?", key: "invalid-key" });
+    
+        expect(response.status).toBe(401);
+        expect(response.body.error.message).toEqual(expect.stringContaining("Incorrect API key provided"));
     });
 
 });
