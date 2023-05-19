4ad62431cb0fb99abd1940698b6395411fa45e99
james5418 <james900425@gmail.com>
2023-05-19 14:59:39 +0800

    [fix] body-parser deprecated undefined extended

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ import { summaryRouter } from './routes/summary';
 const app = express();
 
 app.use(express.json({limit: '50mb'}));
-app.use(express.urlencoded({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb', extended: true}));
 app.use(cors());
 app.use(morgan('common')); 
 app.use("/openai/summary", summaryRouter);
